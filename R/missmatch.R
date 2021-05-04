7a9e2d4f1e5139b57e9490015c3036882150eef9
shossen1 <57533234+shossen1@users.noreply.github.com>
2021-05-04 11:15:41 -0400

    missmatch

---
+++ R/missmatch.R
@@ -0,0 +1,7 @@
+missmatch = function(a,b){
+  print("Present in dataset A but absent in dataset B")
+  names(a)[!(names(a) %in% names(b))]
+
+  print("Present in dataset B but absent in dataset A")
+  names(b)[!(names(b) %in% names(a))]
+}
