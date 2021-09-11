8c090ced528f8ec9048999f263d00a3026e75644
shossen1 <57533234+shossen1@users.noreply.github.com>
2021-09-11 18:03:25 -0400

    cat

--- R/cat_or_cont.R
+++ R/cat_or_cont.R
@@ -1,7 +1,6 @@
 
 
-
 cat_or_cont = function(var){
   xx = length(unique(var))/length(na.omit(var))
-  if(xx <= 0.05 | length(unique(var))>= 10){return("Categorical")} else {return("Continuous")}
+  if(xx <= 0.05 & length(unique(var)) < 12){return("Categorical")} else {return("Continuous")}
 }
