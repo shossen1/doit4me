c15462d2a6ef7714fd7d79fc2dcf98a74e6a1036
shossen1 <57533234+shossen1@users.noreply.github.com>
2021-12-06 01:16:07 -0500

    Create Table1x.R

---
+++ R/Table1x.R
@@ -0,0 +1,47 @@
+table1x = function(datain, varlist, group){
+  tab1 = NULL
+  for(i in varlist){
+    print(i)
+    datain$var = datain[[gsub("[()]", "", gsub('factor', '', i))]]
+    datain$group = datain[[gsub("[()]", "", gsub('factor', '', group))]]
+
+    if(grepl("factor", i) == "FALSE"){
+      xx = data.frame(datain %>%
+                        dplyr::group_by(group) %>%
+                        dplyr::summarise(mean = mean(as.numeric(var), na.rm = TRUE),
+                                         sd = sd(as.numeric(var), na.rm = TRUE)) %>%
+                        dplyr::mutate(meansd = paste0(sprintf("%3.1f", mean), " (", sprintf("%3.1f", sd), ")")) %>%
+                        dplyr::select(meansd) %>%
+                        t())
+      rownames(xx) = paste0(i, " mean(SD)")
+      colnames(xx) = names(table(datain$group))
+
+      xx$Overall = paste0(sprintf("%3.1f", mean(datain$var, na.rm = TRUE)), " (",
+                          sprintf("%3.1f", sd(datain$var, na.rm = TRUE)), ")")
+
+      xx$p = substr(format(tidy(aov(var ~ group, data = datain))$p.value[1], scientific = F), 1, 6)
+      #print(names(xx))
+    }
+
+    if(grepl("factor", i) == "TRUE"){
+      xx = data.frame(datain %>%
+                        tabyl(var, group) %>%
+                        adorn_totals("col") %>%
+                        adorn_percentages("col") %>%
+                        adorn_pct_formatting(digits = 1) %>%
+                        adorn_ns(position = "front") %>%
+                        dplyr::rename(Overall = Total))
+
+      p = substr(format(chisq.test(datain$var, datain$group)$p.value , scientific = F), 1, 6)
+      xx$var = paste0(i , " (", xx$var, ")")
+
+      rownames(xx) = xx$var
+      xx$p = p
+      xx$var = NULL
+      #print(names(xx))
+    }
+    names(xx) = c(names(table(datain$group)), "Overall","p")
+    tab1 = rbind(tab1, xx)
+  }
+  return(tab1)
+}
